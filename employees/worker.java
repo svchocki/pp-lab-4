c8c80973dfae44ee9d4da827f54024a22f509228
svchocki <145773118+svchocki@users.noreply.github.com>
2024-04-14 11:46:35 +0200

    Create worker.java

---
+++ employees/worker.java
@@ -0,0 +1,22 @@
+// Worker.java
+public class Worker extends Employee {
+    public String position;
+
+    public Worker(String fullName, double salary) {
+        super(fullName, salary);
+        this.position = "";
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker: " + getFullName() + ", Salary: " + getSalary() + ", Position: " + position;
+    }
+}
