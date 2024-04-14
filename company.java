4cd7fd33b90976eac5d81719e835f1ea45e51d85
svchocki <145773118+svchocki@users.noreply.github.com>
2024-04-14 11:53:48 +0200

    Update company.java

--- company.java
+++ company.java
@@ -4,18 +4,23 @@ public class Company {
         Employee[] employees = new Employee[5];
 
         employees[0] = new Manager("Jan Manager", 70000);
-        ((Manager) employees[0]).setNumberOfSubordinates(0);
-
-        employees[1] = new Worker("Anna Worker", 45000);
-        ((Worker) employees[1]).setPosition("Assistant");
-
-        employees[2] = new Employee("Piotr Employee", 55000);
-
-        employees[3] = new Worker("Krzysztof Worker", 50000);
-        ((Worker) employees[3]).setPosition("Technician");
-
+        employees[1] = new Worker("Piotr Worker", 45000);
+        employees[2] = new Employee("Krzysztof Employee", 55000);
+        employees[3] = new Worker("Anna Worker", 50000);
         employees[4] = new Worker("Katarzyna Worker", 48000);
-        ((Worker) employees[4]).setPosition("Clerk");
+
+        int nonManagerCount = 0;
+        for (Employee emp : employees) {
+            if (!(emp instanceof Manager)) {
+                nonManagerCount++;
+            }
+        }
+
+        if (employees[0] instanceof Manager) {
+            ((Manager) employees[0]).setNumberOfSubordinates(nonManagerCount);
+        }
+
+        employees[0].setSalary(75000);
 
         System.out.println("Data for all employees:");
         for (Employee emp : employees) {
